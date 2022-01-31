96eb906ab60117cb75c522cb85c83ddd684cc63d
Vasyl-mitityk <Vipsvob@gmail.com>
2022-01-31 20:15:36 +0200

    com-1

---
+++ controllers/contacts/postContact.js
@@ -0,0 +1,9 @@
+const { addContact } = require("../../model");
+
+const postContact = async (req, res, next) => {
+
+  
+  const newContact = await addContact(req.body);
+  res.status(201).json(newContact);
+};
+module.exports = postContact;
